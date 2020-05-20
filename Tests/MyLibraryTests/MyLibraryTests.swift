3d20222985d2bde129a9a5880b49087df4c32bdc
Singh, Navjot <nav_singh@manulife.ca>
2020-05-20 18:52:59 -0400

    all package done

--- Tests/MyLibraryTests/MyLibraryTests.swift
+++ Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,12 +1,23 @@
 import XCTest
 @testable import MyLibrary
+import SwiftUI
 
 final class MyLibraryTests: XCTestCase {
+    @State private var text:String
+    
+    public init(text:String) {
+        self.text = text
+        super.init()
+    }
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyLibrary().text, "Hello, World!")
+        //XCTAssertEqual(MyLibrary().text, "Hello, World!")
+        var body: some View{
+            MyLibrary.MainTextField(placeholder:"Testing", text: $text)
+        }
     }
 
     static var allTests = [
