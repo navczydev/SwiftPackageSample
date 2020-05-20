ae59ce5447e088a04b9f2020a4307af36070d60b
Singh, Navjot <nav_singh@manulife.ca>
2020-05-20 18:25:15 -0400

    Added changes too package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibrary",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MyLibrary",
+            type: .dynamic,
             targets: ["MyLibrary"]),
     ],
     dependencies: [
